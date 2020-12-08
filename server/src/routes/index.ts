973bc0cec02e308ec5e3f086b25ca4608281473c
lleonesouza <lleonesouza@live.com>
2020-12-07 21:25:58 -0300

    add robots routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,11 +1,20 @@
 
 import controllers from "../controllers/index";
 
-const {item, user} = controllers
+const { item, user } = controllers
 
 const makeRoutes = (router: any) => {
-  router.get("/", item.get);
+  router.get("/robots", item.get);
+  // router.get("/robots", item.delete);
+  router.get("/robots", item.put);
+  router.get("/robots", item.post);
+
+
   router.get("/user", user.get);
+  router.delete("/user", user.delete);
+  router.post("/user", user.post);
+  router.put("/user", user.put);
+
 
   return router;
 };
